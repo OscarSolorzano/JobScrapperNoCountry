b8320c1ff1bd5b5cb769ed42b1f286440abee6b0
Oscar Solorzano <oscar.solorzano98@gmail.com>
2022-12-05 17:14:58 -0500

    Time updated

--- scraper/index.js
+++ scraper/index.js
@@ -1,30 +1,32 @@
 const cron = require('cron');
 const { busquedaComputrabajo } = require('./scraperComputrabajo');
-const { busquedaBumeran } = require('./scraperBumeran')
+const { busquedaBumeran } = require('./scraperBumeran');
 
-const cronJobScrapper = () =>{ new cron.CronJob('15 14 1-31 0-11 0-6', async () => {
-    console.log('Ejecutando...');
-    console.log(new Date().toLocaleString())
-    // busqueda('desarrollador backend trainee', '');
-    await busquedaBumeran('backend','')
-    await busquedaBumeran('frontend','')
-    await busquedaComputrabajo('desarrollador frontend', '');
-    // busqueda('desarrollador trainee', '');
-    // busqueda('desarrollador junior', '');
-    await busquedaComputrabajo('desarrollador backend', '');
-    // busqueda('desarrollador backend trainee', '');
-    // busqueda('desarrollador', '');
-    console.log('Finished');
-    console.log(new Date().toLocaleString())
-
-},
-null,
-true);
-}
+const cronJobScrapper = () => {
+  new cron.CronJob(
+    '18 14 1-31 0-11 0-6',
+    async () => {
+      console.log('Ejecutando...');
+      console.log(new Date().toLocaleString());
+      // busqueda('desarrollador backend trainee', '');
+      await busquedaBumeran('backend', '');
+      await busquedaBumeran('frontend', '');
+      await busquedaComputrabajo('desarrollador frontend', '');
+      // busqueda('desarrollador trainee', '');
+      // busqueda('desarrollador junior', '');
+      await busquedaComputrabajo('desarrollador backend', '');
+      // busqueda('desarrollador backend trainee', '');
+      // busqueda('desarrollador', '');
+      console.log('Finished');
+      console.log(new Date().toLocaleString());
+    },
+    null,
+    true
+  );
+};
 
 // const cronJobScrapper = () =>{
 //     busquedaBumeran('backend','')
 // }
 
-
-module.exports = { cronJobScrapper }
+module.exports = { cronJobScrapper };
